6265ec6b89c514179914accf38be738ef3f6fbce
chkim <chokim@xionits.com>
2024-02-16 15:12:56 +0900

    /api/data modified

--- index.js
+++ index.js
@@ -6,23 +6,42 @@ const port = 3001; // 포트 설정
 
 app.use(cors());
 
+const dataLength = 100000
+
+// temp DB queried Data
 const cveTableData = {
-  data: Array.from({ length: 50 }, (_, i) => ({
+  data: Array.from({ length: dataLength }, (_, i) => ({
     componentType: 'Python' + (i + 1),
     componentName: 'pip',
     componentVersion: '9.0.3',
-    cveId: 'CVE-0000-00001',
+    cveId: `CVE-0000-0000${i}`,
     severity: 10.2,
     severityGroups: 'Critical',
     assetCount: 3,
   })),
   meta: {
-    totalRowCount: 50,
+    totalRowCount: dataLength,
   },
 };
 
 app.get('/api/data', (req, res) => {
-  res.send(cveTableData);
+  console.log(req.query)
+  const { start, size, filters, globalFilter, sorting } = req.query
+
+  console.log('filters', filters)
+  console.log('globalFilter', globalFilter)
+  console.log('sorting',sorting)
+
+  const result = {
+    data: cveTableData.data.slice(Number(start), Number(start)+Number(size)),
+    meta:cveTableData.meta
+  }
+
+  console.log(result.data.length)
+
+  res.send(result);
+
+  console.log('-----------------')
 });
 
 app.listen(port, () => {
